c571a3cd2519543a98c1b0a6af524a222b648dd0
lenyn15 <201514029@uns.edu.pe>
2021-02-20 07:52:27 -0500

    Code modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import ComponenteCliente                  from "./ClienteComponent/ComponenteCliente";
 import ListarClientes                     from "./ClienteComponent/ListarClientes";
 import ListarTipos                        from "./ClienteComponent/ListarTipos";
+import ListarDetalles                     from "./ClienteComponent/ListarDetalles";
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                     <Route path="/component-clientes" exact component={ ComponenteCliente }/>
                     <Route path="/view-clientes" exact component={ ListarClientes }/>
                     <Route path="/view-tipos" exact component={ ListarTipos }/>
+                    <Route path="/view-detalles" exact component={ ListarDetalles }/>
                 </div>
             </Router>
         </div>
---
+++ frontend/src/ClienteComponent/ListarDetalles.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class ListarDetalles extends Component {
+
+    constructor( props ) {
+        super( props );
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="title-details">
+                    <h2 className="title-main">Detalles de los clientes</h2>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ListarDetalles
