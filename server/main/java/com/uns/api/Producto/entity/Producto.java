f043a3a607b2cce6fcfe9f3bf15cdb0066fd53e5
lenyn15 <201514029@uns.edu.pe>
2021-02-22 19:38:32 -0500

    Code modified

--- server/main/java/com/uns/api/Producto/entity/Producto.java
+++ server/main/java/com/uns/api/Producto/entity/Producto.java
@@ -15,6 +15,30 @@ public class Producto {
 	         updatable = false )
 	private int id;
 	
+	@Column( name = "nombre",
+	         length = 45,
+	         nullable = false )
+	private String nombre;
+	
+	@Column( name = "precio_compra",
+	         columnDefinition = "DECIMAL(7,2)",
+	         nullable = false )
+	private Double precioCompra;
+	
+	@Column( name = "precio_venta",
+	         columnDefinition = "DECIMAL(7,2)",
+	         nullable = false )
+	private Double precioVenta;
+	
+	@Column( name = "cantidad",
+	         nullable = false )
+	private int cantidad;
+	
+	@Column( name = "estado",
+	         length = 45,
+	         nullable = false )
+	private String estado;
+	
 	@ManyToOne( fetch = FetchType.EAGER )
 	@JoinColumn( name = "categoria_id",
 	             foreignKey = @ForeignKey( name = "fk_categoria" ),
@@ -36,6 +60,46 @@ public class Producto {
 	
 	public void setId( int id ) {
 		this.id = id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre( String nombre ) {
+		this.nombre = nombre;
+	}
+	
+	public Double getPrecioCompra() {
+		return precioCompra;
+	}
+	
+	public void setPrecioCompra( Double precioCompra ) {
+		this.precioCompra = precioCompra;
+	}
+	
+	public Double getPrecioVenta() {
+		return precioVenta;
+	}
+	
+	public void setPrecioVenta( Double precioVenta ) {
+		this.precioVenta = precioVenta;
+	}
+	
+	public int getCantidad() {
+		return cantidad;
+	}
+	
+	public void setCantidad( int cantidad ) {
+		this.cantidad = cantidad;
+	}
+	
+	public String getEstado() {
+		return estado;
+	}
+	
+	public void setEstado( String estado ) {
+		this.estado = estado;
 	}
 	
 	public Categoria getCategoria() {
